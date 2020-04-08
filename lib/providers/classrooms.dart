9e3f4bc4494d4788730b947e318d0f68e8f1e42c
shunjid <shunjid1862@gmail.com>
2020-04-09 00:48:58 +0600

    added notifier method: getClassrooms

--- lib/providers/classrooms.dart
+++ lib/providers/classrooms.dart
@@ -1,4 +1,31 @@
+import 'package:eluminousmobile/models/classroom.dart';
 import 'package:flutter/cupertino.dart';
 
 class Classrooms with ChangeNotifier {
+  /*
+  ** @classrooms : a getter that returns the copied
+  ** data from the list annotated as _classrooms from API
+   */
+  List<Classroom> get classrooms {
+    return [..._classrooms];
+  }
+
+  List<Classroom> _classrooms = [
+    Classroom(
+      classroomId: 0,
+      classroomTitle: 'Dhaka College (Physics)',
+      classroomSection: 'A',
+      classroomShift: 'Morning',
+      accessCode: 'neqy71',
+      enrolledTotal: 21,
+    ),
+    Classroom(
+      classroomId: 0,
+      classroomTitle: 'Dhaka College (Physics)',
+      classroomSection: 'A',
+      classroomShift: 'Morning',
+      accessCode: '9xmHaq',
+      enrolledTotal: 29,
+    ),
+  ];
 }
